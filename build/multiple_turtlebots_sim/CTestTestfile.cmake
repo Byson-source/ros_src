56940ba8bd4554cf260d967a311d35de3cce3db7
Byson-source <byson0713@gmail.com>
2021-07-14 18:18:02 +0900

    2021/07/14

---
+++ build/multiple_turtlebots_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ayumi/catkin_ws/src/multiple_turtlebots_sim
+# Build directory: /home/ayumi/catkin_ws/build/multiple_turtlebots_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlebot_description/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ayumi/catkin_ws/src/turtlebot_description
+# Build directory: /home/ayumi/catkin_ws/build/turtlebot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_turtlebot_description_gtest_turtlebot_description_test_urdf "/home/ayumi/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/local/bin/python3" "/opt/ros/noetic/share/catkin/cmake/test/run_tests.py" "/home/ayumi/catkin_ws/build/test_results/turtlebot_description/gtest-turtlebot_description_test_urdf.xml" "--return-code" "/home/ayumi/catkin_ws/devel/lib/turtlebot_description/turtlebot_description_test_urdf --gtest_output=xml:/home/ayumi/catkin_ws/build/test_results/turtlebot_description/gtest-turtlebot_description_test_urdf.xml")
+set_tests_properties(_ctest_turtlebot_description_gtest_turtlebot_description_test_urdf PROPERTIES  _BACKTRACE_TRIPLES "/opt/ros/noetic/share/catkin/cmake/test/tests.cmake;160;add_test;/opt/ros/noetic/share/catkin/cmake/test/gtest.cmake;98;catkin_run_tests_target;/opt/ros/noetic/share/catkin/cmake/test/gtest.cmake;37;_catkin_add_google_test;/home/ayumi/catkin_ws/src/turtlebot_description/CMakeLists.txt;25;catkin_add_gtest;/home/ayumi/catkin_ws/src/turtlebot_description/CMakeLists.txt;0;")
