0b2c877dfd48c9ec777a4ded247dff30aa20763f
Byson-source <byson0713@gmail.com>
2021-07-20 16:14:19 +0900

    2021/07/20

---
+++ build/collaborate/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ayumi/catkin_ws/src/collaborate
+# Build directory: /home/ayumi/catkin_ws/build/collaborate
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- src/realtime_slam/scripts/transmitter.py
+++ src/realtime_slam/scripts/transmitter.py
@@ -1,16 +1,47 @@
+#! /usr/bin/env python
 import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
-axes,buttons=[],[]
+axes,buttons=[0,0],[0,0]
 def perceive(data):
     global axes,buttons
     axes=data.axes
     buttons=data.buttons
-
+    # print(buttons)
 
 def transmit():
+    global axes,buttons
+    if buttons[0]==1:
+        if axes[-1]==1.0:
+            cmd.linear.x=0.5
+        elif axes[-1]==-1.0:
+            cmd.linear.x = -0.5
+        elif axes[-2]==1.0:
+            cmd.angular.z=0.4
+        elif axes[-2]==-1.0:
+            cmd.angular.z = -0.4
+        else:
+            cmd.linear.x = 0
+            cmd.angular.z = 0
+    elif buttons[1]==1:
+        if axes[-1]==1.0:
+            cmd.linear.x=1.5
+        elif axes[-1]==-1.0:
+            cmd.linear.x = -1.5
+        elif axes[-2]==1.0:
+            cmd.angular.z=0.4
+        elif axes[-2]==-1.0:
+            cmd.angular.z = -0.4
+        else:
+            cmd.linear.x = 0
+            cmd.angular.z = 0
+    else:
+        cmd.linear.x=0
+        cmd.angular.z=0
+
     command.publish(cmd)
     rate.sleep()
+
 
 if __name__ == '__main__':
     rospy.init_node('Operation_com')
@@ -22,3 +53,5 @@ if __name__ == '__main__':
 
     while True:
         transmit()
+
+    rospy.spin()
