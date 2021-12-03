516621ea555a72ae8bd85ea21d080ae6bd79ed19
Byson-source <byson0713@gmail.com>
2021-12-03 19:53:33 +0900

    rosbag改造中

--- src/cpp/scripts/frame_change.py
+++ src/cpp/scripts/frame_change.py
@@ -1,5 +1,3 @@
-
-
 import sys
 import os
 import rosbag
@@ -7,34 +5,21 @@ from geometry_msgs.msg import TransformStamped
 # PKG = 'bag_tools'  # this package name
 
 # roslib.load_manifest(PKG)
-
-INPUTDATABASE = "/home/ayumi/Documents/RTAB-Map/map2.bag"
-OUTPUTDATABASE1 = "/home/ayumi/Documents/RTAB-Map/map2_1.bag"
-OUTPUTDATABASE2 = "/home/ayumi/Documents/RTAB-Map/map2_2.bag"
-OUTPUTDATABASE3 = "/home/ayumi/Documents/RTAB-Map/map2_3.bag"
-OUTPUTDATABASE4 = "/home/ayumi/Documents/RTAB-Map/map2_4.bag"
-OUTPUTDATABASE5 = "/home/ayumi/Documents/RTAB-Map/map2_5.bag"
-OUTPUTDATABASE6 = "/home/ayumi/Documents/RTAB-Map/map2_6.bag"
-
-
-out=[INPUTDATABASE,OUTPUTDATABASE1,OUTPUTDATABASE2,OUTPUTDATABASE3,OUTPUTDATABASE4, OUTPUTDATABASE5]
-
-NAMESPACE = "robot2/"
-NAME="/robot2"
-topic_list = ["base_controller/odom", "base_scan", "data_throttled_camera_info",
-              "data_throttled_image/compressed", "data_throttled_image_depth/compressedDepth"]
-goal_frame = ["odom", "base_laser_link", "camera_rgb_optical_frame",
-              "camera_rgb_optical_frame", "camera_rgb_optical_frame"]
+NAMESPACE = "robot1/"
+NAME="/robot1"
+topic_list = ["camera/depth/image",
+              "camera/rgb/camera_info", "camera/rgb/image_color/compressed"]
+goal_frame = "camera_rgb_optical_frame"
 
 
 
-def change_frame_id(inbag, outbag, frame_val, topic_):
+def change_frame_id(inbag, outbag):
     print("Please wait...")
     i=0
     outbag = rosbag.Bag(outbag, 'w')
     for topic, msg, t in rosbag.Bag(inbag, 'r').read_messages():
-        if topic_ in topic:
-            msg.child_frame_id = NAMESPACE+frame_val
+        if ((topic_list[0] in topic) or (topic_list[1] in topic) or (topic_list[2] in topic)):
+            msg.child_frame_id = NAMESPACE+goal_frame
             print(msg.child_frame_id)
         outbag.write(topic, msg, t)
     i+=1
@@ -51,10 +36,12 @@ def two_round(outbag2):
     
     print('Closing output bagfile and exit...')
     outbag2.close()
-# i=0
-# for frame_val in goal_frame:
-#     change_frame_id(out[i], out[i+1], frame_val, topic_list[i])
-#     i+=1
-# two_round(OUTPUTDATABASE6)
-change_frame_id("/home/ayumi/Documents/RTAB-Map/out2_.bag", "/home/ayumi/Documents/RTAB-Map/out2.bag", 
-"base_footprint", "/base_controller/odom")
+i=0
+for frame_val in goal_frame:
+    change_frame_id(out[i], out[i+1], frame_val, topic_list[i])
+    i+=1
+    
+    
+two_round(OUTPUTDATABASE6)
+change_frame_id("/home/ayumi/Documents/bag_dataset/Arc.bag",
+                "/home/ayumi/Documents/bag_dataset/Arc1.bag")
