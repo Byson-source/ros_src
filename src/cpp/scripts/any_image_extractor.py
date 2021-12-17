7267ac318bedce23da96bf2dbdd97b9cd467c38c
Byson-source <byson0713@gmail.com>
2021-12-17 19:59:46 +0900

    何かがおかしい

---
+++ src/cpp/scripts/any_image_extractor.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python
+import rospy
+# ROS Image message
+from sensor_msgs.msg import Image
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+# OpenCV2 for saving an image
+from rtabmap_ros.msg import Info
+
+from std_msgs.msg import Int32
+
+import message_filters
+
+import cv2
+import time
+
+path = "/home/ayumi/Documents/tohoku_uni/CLOVERs/images/"
+
+all_rgb = "/home/ayumi/Documents/tohoku_uni/CLOVERs/images/all_rgb/"
+
+rospy.init_node('image_listener', anonymous=True)
+
+total_num = rospy.get_param("~total")
+img_number = rospy.get_param("~robot_number")
+# Instantiate CvBridge
+bridge = CvBridge()
+state=1
+state_beta=1
+
+def callback(rgb, id):
+    global img_number,state,state_beta
+    
+    if state_beta==1:
+
+        cv2_img = bridge.imgmsg_to_cv2(rgb, "bgr8")
+        cv2_img = cv2.resize(cv2_img, dsize=(512, 384))
+        
+        cv2.imwrite(path + str(state)+"_rgb/"+str(img_number) + ".jpg", cv2_img)
+        cv2.imwrite(all_rgb + str(img_number) + ".jpg", cv2_img)
+        
+        img_number += total_num
+        state_beta=0
+    # こちらのノードのほうが早く動くと、同じディレクトリに連続で写真を保存してしまうため
+    else:
+        pass
+    
+
+def changeCB(data):
+    global state,state_beta
+    state_beta=1
+    if state==1:
+        state=2
+    else:
+        state=1
+
+rgb_topic = "/robot"+str(img_number)+"/camera/rgb/image_raw"
+ID_topic = "/robot"+str(img_number)+"/rtabmap/info"
+# # To store the images from camera
+rgb_sub = message_filters.Subscriber(rgb_topic, Image)
+myid_sub = message_filters.Subscriber(ID_topic, Info)
+
+path_changer=rospy.Subscriber("imagepath_change",Int32,changeCB)
+
+# ts = message_filters.TimeSynchronizer([rgb_sub, myid_sub], 10)
+ts=message_filters.ApproximateTimeSynchronizer([rgb_sub,myid_sub],10,0.1,allow_headerless=False)
+ts.registerCallback(callback)
+# rospy.Subscriber(image_topic, Image, rgb_callback)
+rospy.spin()
--- src/cpp/scripts/image_extractor.py
+++ src/cpp/scripts/image_extractor.py
@@ -15,21 +15,17 @@ import cv2
 import time
 
 path = "/home/ayumi/Documents/tohoku_uni/CLOVERs/images/"
+
 all_rgb = "/home/ayumi/Documents/tohoku_uni/CLOVERs/images/all_rgb/"
 
 rospy.init_node('image_listener', anonymous=True)
 
-robot_number = rospy.get_param("~robot_number")
 total_num = rospy.get_param("~total")
 
-img_number=robot_number
-img_number = robot_number
-
-
+img_number=1
 # Instantiate CvBridge
 bridge = CvBridge()
 state={}
-dir=1
 
 def callback(rgb, id):
     global img_number,state
@@ -37,7 +33,7 @@ def callback(rgb, id):
     cv2_img = bridge.imgmsg_to_cv2(rgb, "bgr8")
     cv2_img = cv2.resize(cv2_img, dsize=(512, 384))
     
-    cv2.imwrite(path + str(state)+"_rgb"+str(img_number) + ".jpg", cv2_img)
+    cv2.imwrite(path + str(state)+"_rgb/"+str(img_number) + ".jpg", cv2_img)
     cv2.imwrite(all_rgb + str(img_number) + ".jpg", cv2_img)
     
     img_number += total_num
@@ -45,7 +41,7 @@ def callback(rgb, id):
     state["first"]=1
     
     while (("second" not in state) or (state["second"]==0)):
-        time.sleep(0.0000001)
+        time.sleep(0.0001)
         
     int_msg=Int32()
     int_msg.data=1
@@ -56,23 +52,23 @@ def callback(rgb, id):
     
 def state_CB(id):
     global state
+    print("Switch!!")
     state["second"]=1
 
 rgb_topic = "/robot1/camera/rgb/image_raw"
 ID_topic = "/robot1/rtabmap/info"
-
-rgb_topic2 = "/robot2/camera/rgb/image_raw"
-ID_topic2 = "/robot2/rtabmap/info"
+monitor_id="/robot2/rtabmap/info"
 # # To store the images from camera
 rgb_sub = message_filters.Subscriber(rgb_topic, Image)
 
 myid_sub = message_filters.Subscriber(ID_topic, Info)
 
-id_monitor = rospy.Subscriber(ID_topic, Info, state_CB)
+id_monitor = rospy.Subscriber(monitor_id, Info, state_CB)
 
-path_changer=rospy.Publisher("comd_about_imagepath",Int32,queue_size=1)
+path_changer=rospy.Publisher("imagepath_change",Int32,queue_size=1)
 
-ts = message_filters.TimeSynchronizer([rgb_sub, myid_sub], 10)
+# ts = message_filters.TimeSynchronizer([rgb_sub, myid_sub], 10)
+ts=message_filters.ApproximateTimeSynchronizer([rgb_sub,myid_sub],10,0.1,allow_headerless=False)
 ts.registerCallback(callback)
 # rospy.Subscriber(image_topic, Image, rgb_callback)
 rospy.spin()
