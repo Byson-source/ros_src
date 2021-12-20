36bf683758903747048d15ae13513b17233e5bcd
Byson-source <byson0713@gmail.com>
2021-12-20 15:57:20 +0900

    image_extractrがこんどこそ正常に交互に保存するようになった

--- src/cpp/scripts/any_image_extractor.py
+++ src/cpp/scripts/any_image_extractor.py
@@ -28,47 +28,52 @@ img_number = rospy.get_param("~robot_number")
 bridge = CvBridge()
 state=1
 state_beta=1
+iteration=0
 
-if not (os.path.isdir(all_rgb) or os.path.isdir(path+"1_rgb") 
-        or os.path.isdir(path+"2_rgb")):
-    time.sleep(0.0001)
 
 def callback(rgb, id):
-    global img_number,state,state_beta
+    global img_number,state,state_beta,iteration
     
     if state_beta==1:
 
         cv2_img = bridge.imgmsg_to_cv2(rgb, "bgr8")
         cv2_img = cv2.resize(cv2_img, dsize=(512, 384))
-        
+
+
         cv2.imwrite(path + str(state)+"_rgb/"+str(img_number) + ".jpg", cv2_img)
         cv2.imwrite(all_rgb + str(img_number) + ".jpg", cv2_img)
         
         img_number += total_num
         state_beta=0
     # こちらのノードのほうが早く動くと、同じディレクトリに連続で写真を保存してしまうため
+        iteration+=1
     else:
         pass
     
 
 def changeCB(data):
-    global state,state_beta
+    global state,state_beta,iteration
     state_beta=1
-    if state==1:
+    if (state==1 and iteration>0):
         state=2
     else:
         state=1
 
-rgb_topic = "/robot"+str(img_number)+"/camera/rgb/image_raw"
-ID_topic = "/robot"+str(img_number)+"/rtabmap/info"
-# # To store the images from camera
-rgb_sub = message_filters.Subscriber(rgb_topic, Image)
-myid_sub = message_filters.Subscriber(ID_topic, Info)
+if __name__=='__main__':
 
-path_changer=rospy.Subscriber("imagepath_change",Int32,changeCB)
+    if not (os.path.isdir(all_rgb) or os.path.isdir(path+"1_rgb")
+            or os.path.isdir(path+"2_rgb")):
+        time.sleep(0.0001)
+    rgb_topic = "/robot"+str(img_number)+"/camera/rgb/image_raw"
+    ID_topic = "/robot"+str(img_number)+"/rtabmap/info"
+    # # To store the images from camera
+    rgb_sub = message_filters.Subscriber(rgb_topic, Image)
+    myid_sub = message_filters.Subscriber(ID_topic, Info)
 
-# ts = message_filters.TimeSynchronizer([rgb_sub, myid_sub], 10)
-ts=message_filters.ApproximateTimeSynchronizer([rgb_sub,myid_sub],10,0.1,allow_headerless=False)
-ts.registerCallback(callback)
-# rospy.Subscriber(image_topic, Image, rgb_callback)
-rospy.spin()
+    path_changer=rospy.Subscriber("imagepath_change",Int32,changeCB)
+
+    # ts = message_filters.TimeSynchronizer([rgb_sub, myid_sub], 10)
+    ts=message_filters.ApproximateTimeSynchronizer([rgb_sub,myid_sub],10,0.1,allow_headerless=False)
+    ts.registerCallback(callback)
+    # rospy.Subscriber(image_topic, Image, rgb_callback)
+    rospy.spin()
--- src/cpp/scripts/image_extractor.py
+++ src/cpp/scripts/image_extractor.py
@@ -1,4 +1,4 @@
-#FIXME You need to get the feedback from loop_closure node that it finishes reading two images in that directory
+# FIXME You need to get the feedback from loop_closure node that it finishes reading two images in that directory
 # NOTE This node works only for robot1. And gives commands to any_image_extractorand loop closure node
 #! /usr/bin/python
 import rospy
@@ -11,6 +11,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 # OpenCV2 for saving an image
 from rtabmap_ros.msg import Info
+from std_msgs.msg import String
 
 from std_msgs.msg import Int32
 
@@ -30,75 +31,98 @@ os.mkdir(all_rgb)
 os.mkdir(path+"/1_rgb/")
 os.mkdir(path+"/2_rgb/")
 
-
 rospy.init_node('image_listener', anonymous=True)
 
 total_num = rospy.get_param("~total")
 
-img_number=1
+img_number = 1
 # Instantiate CvBridge
 bridge = CvBridge()
-state={}
-dir_num=1
+state = {}
+dir_num = 1
+
+iteration = 0
+
 
 def callback(rgb, id):
-    global img_number,state,dir_num
+    global img_number, state, dir_num, already_loop, iteration, path_changer
+
+    iteration += 1
 
     cv2_img = bridge.imgmsg_to_cv2(rgb, "bgr8")
 # FIXME Maybe should change to (640,480)
     cv2_img = cv2.resize(cv2_img, dsize=(512, 384))
-    
+
     cv2.imwrite(path + str(dir_num)+"_rgb/"+str(img_number) + ".jpg", cv2_img)
     cv2.imwrite(all_rgb + str(img_number) + ".jpg", cv2_img)
-    
+
     img_number += total_num
-    
-    state["first"]=1
-    
-    while (("second" not in state) or (state["second"]==0)):
+
+    state["first"] = 1
+
+    while (("second" not in state) or (state["second"] == 0)):
         time.sleep(0.0001)
 
-    # Remove stored images
-    os.remove(path + str(dir_num)+"_rgb/"+str(img_number) + ".jpg")
-    try:
-        os.remove(path + str(dir_num)+"_rgb/"+str(img_number+1) + ".jpg")
-    except FileNotFoundError:
-        os.remove(path + str(dir_num)+"_rgb/"+str(img_number-1) + ".jpg")
-    
-    if dir_num==1:dir_num=2
-    else:dir_num=1
-    
-    int_msg=Int32()
-    int_msg.data=1
+    int_msg = Int32()
+    int_msg.data = 1
     path_changer.publish(int_msg)
-    rate=rospy.Rate(100)
-    rate.sleep()
-    
 
+    # while(already_loop==0):
+    #     time.sleep(0.0001)
+
+    # Remove stored images
+    # if (iteration > 1):
+    #     if dir_num == 1:
+    #         os.remove(path + "2_rgb/"+str(img_number-2) + ".jpg")
+    #         os.remove(path + "2_rgb/"+str(img_number-1) + ".jpg")
+    #     if dir_num == 2:
+    #         print(img_number)
+    #         os.remove(path + "1_rgb/"+str(img_number-2) + ".jpg")
+    #         os.remove(path + "1_rgb/"+str(img_number-1) + ".jpg")
+    print(img_number)
+    if dir_num == 1:
+        dir_num = 2
+    else:
+        dir_num = 1
+
+    rate = rospy.Rate(100)
+    rate.sleep()
     # Define your image topic
-    
+
+
 def state_CB(id):
     global state
-    state["second"]=1
+    state["second"] = 1
 
-rgb_topic = "/robot1/camera/rgb/image_raw"
-ID_topic = "/robot1/rtabmap/info"
-#FIXME Fix to make it more easy to use for any-number usage
-monitor_id="/robot2/rtabmap/info"
-# # To store the images from camera
 
-# Remove all image
+# NOTE wait until detection finishes
+def loop_CB(loop):
+    global already_loop
+    already_loop=1
 
-rgb_sub = message_filters.Subscriber(rgb_topic, Image)
+if __name__ == '__main__':
 
-myid_sub = message_filters.Subscriber(ID_topic, Info)
+    rgb_topic = "/robot1/camera/rgb/image_raw"
+    ID_topic = "/robot1/rtabmap/info"
+    # FIXME Fix to make it more easy to use for any-number usage
+    monitor_id = "/robot2/rtabmap/info"
+    # # To store the images from camera
 
-id_monitor = rospy.Subscriber(monitor_id, Info, state_CB)
+    # Remove all image
 
-path_changer=rospy.Publisher("imagepath_change",Int32,queue_size=1)
+    rgb_sub = message_filters.Subscriber(rgb_topic, Image)
 
-# ts = message_filters.TimeSynchronizer([rgb_sub, myid_sub], 10)
-ts=message_filters.ApproximateTimeSynchronizer([rgb_sub,myid_sub],10,0.1,allow_headerless=False)
-ts.registerCallback(callback)
-# rospy.Subscriber(image_topic, Image, rgb_callback)
-rospy.spin()
+    myid_sub = message_filters.Subscriber(ID_topic, Info)
+
+    detect_finish=rospy.Subscriber("loop",String,loop_CB)
+
+    id_monitor = rospy.Subscriber(monitor_id, Info, state_CB)
+
+    path_changer = rospy.Publisher("imagepath_change", Int32, queue_size=1)
+
+    # ts = message_filters.TimeSynchronizer([rgb_sub, myid_sub], 10)
+    ts = message_filters.ApproximateTimeSynchronizer(
+        [rgb_sub, myid_sub], 10, 0.1, allow_headerless=False)
+    ts.registerCallback(callback)
+    # rospy.Subscriber(image_topic, Image, rgb_callback)
+    rospy.spin()
