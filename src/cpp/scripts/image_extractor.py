13a67887a82bb03b6883ee31e584c077a0dd6e47
Byson-source <byson0713@gmail.com>
2021-12-20 19:22:43 +0900

    消さずに交互に入れる

--- src/cpp/scripts/image_extractor.py
+++ src/cpp/scripts/image_extractor.py
@@ -1,5 +1,5 @@
 # FIXME You need to get the feedback from loop_closure node that it finishes reading two images in that directory
-# NOTE This node works only for robot1. And gives commands to any_image_extractorand loop closure node
+# FIXME This works only for few robots. If you want, convert this into C++.
 #! /usr/bin/python
 import rospy
 
@@ -24,28 +24,24 @@ path = "/home/ayumi/Documents/tohoku_uni/CLOVERs/images/"
 
 all_rgb = "/home/ayumi/Documents/tohoku_uni/CLOVERs/images/all_rgb/"
 
-shutil.rmtree(path)
 
-os.mkdir(path)
-os.mkdir(all_rgb)
-os.mkdir(path+"/1_rgb/")
-os.mkdir(path+"/2_rgb/")
-
-rospy.init_node('image_listener', anonymous=True)
-
-total_num = rospy.get_param("~total")
+total_num = 2
 
 img_number = 1
+img_number2=2
+
 # Instantiate CvBridge
 bridge = CvBridge()
 state = {}
+
 dir_num = 1
+dir_num2=1
 
 iteration = 0
 
 
 def callback(rgb, id):
-    global img_number, state, dir_num, already_loop, iteration, path_changer
+    global img_number, state, dir_num, already_loop, iteration
 
     iteration += 1
 
@@ -62,66 +58,89 @@ def callback(rgb, id):
     while (("second" not in state) or (state["second"] == 0)):
         time.sleep(0.0000000000001)
 
-    int_msg = Int32()
-    int_msg.data = 1
+    # if (iteration > 1):
+    #     if dir_num == 1:
+    #         os.remove(path + "2_rgb/"+str(img_number-2) + ".jpg")
+    #         os.remove(path + "2_rgb/"+str(img_number-1) + ".jpg")
+    #     if dir_num == 2:
+    #         os.remove(path + "1_rgb/"+str(img_number-2) + ".jpg")
+    #         os.remove(path + "1_rgb/"+str(img_number-1) + ".jpg")
 
-    # while(already_loop==0):
-    #     time.sleep(0.0001)
-
-    # Remove stored images
-    if (iteration > 1):
-        if dir_num == 1:
-            os.remove(path + "2_rgb/"+str(img_number-2) + ".jpg")
-            os.remove(path + "2_rgb/"+str(img_number-1) + ".jpg")
-        if dir_num == 2:
-            os.remove(path + "1_rgb/"+str(img_number-2) + ".jpg")
-            os.remove(path + "1_rgb/"+str(img_number-1) + ".jpg")
-
-    path_changer.publish(int_msg)
+    
     img_number += total_num
+
     if dir_num == 1:
         dir_num = 2
     else:
         dir_num = 1
 
-    rate = rospy.Rate(100)
-    rate.sleep()
-    # Define your image topic
+
+def callback2(rgb, id):
+    global img_number2, state, dir_num2, already_loop,iteration
+
+    cv2_img = bridge.imgmsg_to_cv2(rgb, "bgr8")
+# FIXME Maybe should change to (640,480)
+    cv2_img = cv2.resize(cv2_img, dsize=(512, 384))
 
 
-def state_CB(id):
-    global state
+    cv2.imwrite(path + str(dir_num2)+"_rgb/"+str(img_number2) + ".jpg", cv2_img)
+    cv2.imwrite(all_rgb + str(img_number2) + ".jpg", cv2_img)
+
+
     state["second"] = 1
 
+    img_number2 += total_num
+
+    if dir_num2 == 1:
+        dir_num2 = 2
+    else:
+        dir_num2 = 1
 
 # NOTE wait until detection finishes
 def loop_CB(loop):
     global already_loop
     already_loop=1
 
+
+
 if __name__ == '__main__':
 
+    shutil.rmtree(path)
+
+    os.mkdir(path)
+    os.mkdir(all_rgb)
+    os.mkdir(path+"/1_rgb/")
+    os.mkdir(path+"/2_rgb/")
+
+    rospy.init_node('image_listener', anonymous=True)
+
+    # NOTE Input topics as many as you want
     rgb_topic = "/robot1/camera/rgb/image_raw"
+    rgb_topic2="/robot2/camera/rgb/image_raw"
+
     ID_topic = "/robot1/rtabmap/info"
     # FIXME Fix to make it more easy to use for any-number usage
-    monitor_id = "/robot2/rtabmap/info"
+    ID_topic2 = "/robot2/rtabmap/info"
     # # To store the images from camera
 
     # Remove all image
 
     rgb_sub = message_filters.Subscriber(rgb_topic, Image)
+    rgb_sub2 = message_filters.Subscriber(rgb_topic2, Image)
 
     myid_sub = message_filters.Subscriber(ID_topic, Info)
+    myid_sub2 = message_filters.Subscriber(ID_topic2, Info)
 
     detect_finish=rospy.Subscriber("loop",String,loop_CB)
 
-    id_monitor = rospy.Subscriber(monitor_id, Info, state_CB)
-
-    path_changer = rospy.Publisher("imagepath_change", Int32, queue_size=1)
 
     # ts = message_filters.TimeSynchronizer([rgb_sub, myid_sub], 10)
     ts = message_filters.ApproximateTimeSynchronizer(
         [rgb_sub, myid_sub], 10, 0.1, allow_headerless=False)
     ts.registerCallback(callback)
+
+    ts2 = message_filters.ApproximateTimeSynchronizer(
+        [rgb_sub2, myid_sub2], 10, 0.1, allow_headerless=False)
+    ts2.registerCallback(callback2)
     # rospy.Subscriber(image_topic, Image, rgb_callback)
     rospy.spin()
