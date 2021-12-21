f0a5a0adf5f624839ac5cb0048f7cdca5d80db64
Byson-source <byson0713@gmail.com>
2021-12-21 18:42:55 +0900

    loopとの折り合いをかんがえてみた。

--- src/cpp/scripts/image_extractor.py
+++ src/cpp/scripts/image_extractor.py
@@ -1,5 +1,8 @@
 # TODO You need to get the feedback from loop_closure node that tells when to start and to restart again.
-# TODO lop検出中に来た画像を保留しておくこと。
+
+# FIXME stockのリカバー中にstockが増える可能性
+# FIXME ファイルを消した後に(start2endが決まった後に）cb2がファイルを新たに作る可能性
+
 # FIXME This works only for few robots. If you want, convert this into C++.
 #! /usr/bin/python
 import rospy
@@ -28,12 +31,15 @@ all_rgb = "/home/ayumi/Documents/tohoku_uni/CLOVERs/images/all_rgb/"
 total_num = 2
 
 img_number = 1
-img_number2=2
+img_number2 = 2
 
-already_loop=1
+already_loop = 1
+# callback1's condition,2's condition
+confirm = 0
 
-stock={}
-start2end={"end":0}
+condition={}
+stock = {}
+start2end = {"end": 0}
 # start ,end,previous_end
 
 # index:img
@@ -41,60 +47,83 @@ start2end={"end":0}
 # Instantiate CvBridge
 bridge = CvBridge()
 
+
 def callback(rgb, id):
-    global img_number,img_number2,already_loop,stock,start2end
+    global img_number, img_number2, already_loop, stock, start2end,condition
 
     cv2_img = bridge.imgmsg_to_cv2(rgb, "bgr8")
 # FIXME Maybe should change to (640,480)
     cv2_img = cv2.resize(cv2_img, dsize=(512, 384))
-    if already_loop==1:
+
+    # 再開
+    if already_loop == 1:
+        condition["cb1 erasing"]=0
+
+
+        if condition["cb1 storing"]==0:
+            condition["cb1 recovering"]=1
+            # NOTE store by callback1 begins!!
+            # stockのセーブ
+            # confirm = len(stock)
+            if "start" in start2end:
+                for index, img in stock:
+                    cv2.imwrite(path + "rgb/"+str(index) + ".jpg", img)
+            condition["cb1 recovering"]=0
+
+        condition["cb1 storing"]=1
         cv2.imwrite(path + "rgb/"+str(img_number) + ".jpg", cv2_img)
         cv2.imwrite(all_rgb + str(img_number) + ".jpg", cv2_img)
 
         img_number += total_num
+
+        stock.clear()
+
+    # 中止
     else:
-        start2end["start"]=start2end["end"]+1
-        if img_number>img_number2:
-            start2end["end"]=img_number
-        else:
-            start2end["end"]=img_number2
+        condition["cb1 storing"]=0
+        if(condition["cb1 erasing"]==0):
+            start2end["start"] = start2end["end"]+1
+            if img_number > img_number2:
+                start2end["end"] = img_number
+            else:
+                start2end["end"] = img_number2
 
-        stock[str(img_number)]=cv2_img
+        condition["cb1 erasing"]=1
+        for file_num in range(start2end["start"], start2end["end"]):
+            os.remove(path + "rgb/"+str(file_num) + ".jpg")
 
+        stock[str(img_number)] = cv2_img
 
 def callback2(rgb, id):
-    global img_number2, already_loop,stock
+    global img_number2, already_loop, stock,condition
 
     cv2_img = bridge.imgmsg_to_cv2(rgb, "bgr8")
 # FIXME Maybe should change to (640,480)
     cv2_img = cv2.resize(cv2_img, dsize=(512, 384))
-
-    if already_loop==1:
+    # 再開
+    if already_loop == 1:
+        condition["cb2 storing"]=1
         cv2.imwrite(path + "rgb/"+str(img_number2) + ".jpg", cv2_img)
         cv2.imwrite(all_rgb + str(img_number2) + ".jpg", cv2_img)
 
         img_number2 += total_num
+    # 中止
     else:
-        stock[str(img_number2)]=cv2_img
-
-    
+        condition["cb2 storing"]=0
+        stock[str(img_number2)] = cv2_img
 
 # NOTE wait until detection finishes
+
+
 def loop_CB(loop):
-    global already_loop,stock,start2end
-    # 再開
-    if already_loop==0:
-        for file_num in range(start2end["start"],start2end["end"]):
-            os.remove(path + "rgb/"+str(file_num) + ".jpg")
-            
-        for index,img in stock:
-            cv2.imwrite(path + "rgb/"+str(index) + ".jpg", img)
-        already_loop=1
+    global already_loop, stock, start2end
+    if already_loop == 0:
+    # 再開    
+        already_loop = 1
 
     else:
-    # 中止
-        already_loop=0
-
+        # 中止
+        already_loop = 0
 
 
 if __name__ == '__main__':
@@ -109,7 +138,7 @@ if __name__ == '__main__':
 
     # NOTE Input topics as many as you want
     rgb_topic = "/robot1/camera/rgb/image_raw"
-    rgb_topic2="/robot2/camera/rgb/image_raw"
+    rgb_topic2 = "/robot2/camera/rgb/image_raw"
 
     ID_topic = "/robot1/rtabmap/info"
     # FIXME Fix to make it more easy to use for any-number usage
@@ -124,8 +153,7 @@ if __name__ == '__main__':
     myid_sub = message_filters.Subscriber(ID_topic, Info)
     myid_sub2 = message_filters.Subscriber(ID_topic2, Info)
 
-    detect_note=rospy.Subscriber("loop",String,loop_CB)
-
+    detect_note = rospy.Subscriber("loop", String, loop_CB)
 
     # ts = message_filters.TimeSynchronizer([rgb_sub, myid_sub], 10)
     ts = message_filters.ApproximateTimeSynchronizer(
---
+++ src/test.py
@@ -0,0 +1,3 @@
+test={}
+test["r"]=1
+print(next(iter(test)))
