40954c7d8c2ce0e3be433b58708c5024b00b2fe0
Byson-source <byson0713@gmail.com>
2022-01-12 13:13:09 +0900

    tf listener作成

--- src/cpp/src/relative_pose_estimation/RO.cpp
+++ src/cpp/src/relative_pose_estimation/RO.cpp
@@ -10,7 +10,10 @@
 #include <vector>
 #include "cpp/MultiArray.h"
 #include "cpp/RO_Array.h"
-#include "tf2_msgs/TFMessage.h"
+#include <math.h>
+#include "calibration.h"
+// FIXME CMakeLists.txtを修正
+#include <tf/transform_listener.h>
 
 class RO_Estimator
 {
@@ -19,47 +22,70 @@ private:
     ros::Subscriber feature_sub;
     ros::Subscriber pose_sub;
 
-    Eigen::Matrix<float,3,4> pose;
+    tf::TransformListener listener1;
+    tf::TransformListener listener2;
 
 public:
     RO_Estimator()
     {
-        feature_sub=n.subscribe("features",20,&RO_estimator::RO_CB,this);
+        feature_sub = n.subscribe("features", 20, &RO_estimator::RO_CB, this);
         // from robot/map to robot/base_footprint
-        pose_sub=n.subscribe("tf",20,&RO_estimator::poseCB,this);
-
     }
 
-    void RO_CB(const std_msgs::cpp::ConstPtr &data){
+    void RO_CB(const std_msgs::cpp::ConstPtr &data)
+    {
+        tf::StampedTransform transform1;
+        tf::StampedTransform transform2;
+        try
+        {
+            listener1.lookupTransform("/robot1/map", "/robot1/base_footprint",
+                                      ros::Time(0), transform1)
+                listener2.lookupTransform("/robot2/map", "/robot2/base_footprint",
+                                          ros::Time(0), transform2)
+        }
+        catch (tf::TransformException &ex)
+        {
+            ROS_ERROR("%s", ex.what());
+            continue;
+        }
+        double x1, y1, z1 = transform1.getOrigin().x(), transform1.getOrigin().y(), transform1.getOrigin().z();
+        double x2, y2, z2 = transform2.getOrigin().x(), transform2.getOrigin().y(), transform2.getOrigin().z();
+        double roll1, pitch1, yaw1;
+        double roll2, pitch2, yaw2;
+        tf::Quaternion q1, q2 = transform1.getRotation(), transform2.getRotation();
+        tf::Matrix3x3(q1).getRPY(roll1, pitch1, yaw1);
+        tf::Matrix3x3(q2).getRPY(roll2, pitch2, yaw2);
+
+        Eigen::Matrix<float, 4, 4> pose1, pose2;
+        pose1 << cos(roll1) * cos(pitch1), cos(roll1) * sin(pitch1) * sin(yaw1) - sin(roll1) * cos(yaw1), cos(roll1) * sin(pitch1) * cos(yaw1) + sin(roll1) * sin(yaw1), x1,
+            sin(roll1) * cos(pitch1), sin(roll1) * sin(pitch1) * sin(yaw1) + cos(roll1) * cos(yaw1), sin(roll1) * sin(pitch1) * cos(yaw1) - cos(roll1) * sin(yaw1), y1,
+            -sin(pitch1), cos(pitch1) * sin(yaw1), cos(pitch1) * cos(yaw1), z1,
+            0, 0, 0, 1;
+        pose2 << cos(roll2) * cos(pitch2), cos(roll2) * sin(pitch2) * sin(yaw2) - sin(roll2) * cos(yaw2), cos(roll2) * sin(pitch2) * cos(yaw2) + sin(roll2) * sin(yaw2), x2,
+            sin(roll2) * cos(pitch2), sin(roll2) * sin(pitch2) * sin(yaw2) + cos(roll2) * cos(yaw2), sin(roll2) * sin(pitch2) * cos(yaw2) - cos(roll2) * sin(yaw2), y2,
+            -sin(pitch2), cos(pitch2) * sin(yaw2), cos(pitch2) * cos(yaw2), z2,
+            0, 0, 0, 1;
+
         // bearing vectors
         Eigen::MatrixXd cov_xx;
         Eigen::MatrixXd cov_ldld;
         points_t points;
         // FIXME pointsの座標はcalibration.hで求める
-        Eigen::MatrixXd gt(3,$numberofPoints);
+        Eigen::MatrixXd gt(3, $numberofPoints);
         // FIXME number of points,店の三次元座標
 
         opengv::bearingVectors_t bearingVectors;
         size_t numberCams{1};
         size_t caamCorrespondence{0};
         // FIXME number of points
-        for( size_t i = 0; i < $Number of points; i++ ){
-            translation_t camOffset =Eigen::Vector3d::Zero();
-            rotation_t camRotation=Eigen::Matrix3d::Identity();
+        for (size_t i = 0; i < $Number of points; i++)
+        {
+            translation_t camOffset = Eigen::Vector3d::Zero();
+            rotation_t camRotation = Eigen::Matrix3d::Identity();
             points.push_back(gt.col(i));
 
-            point_t bodyPoint = rotation.transpose()*(gt.col(i) - position);
+            point_t bodyPoint = rotation.transpose() * (gt.col(i) - position);
             // FIXME multiThreadの必要？？どこかでPoseを知る
         }
-
-        void poseCB(const tf2_msgs::TFMessage &data){
-            
-        }
-
-        
     }
-
-
-
-    
 };
--- src/learning_tf/src/tf_listener.cpp
+++ src/learning_tf/src/tf_listener.cpp
@@ -2,6 +2,8 @@
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Spawn.h>
+#include <iostream>
+
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "my_tf_listener");
@@ -25,6 +27,9 @@ int main(int argc, char** argv){
     try{
       listener.lookupTransform("/turtle2", "/turtle1",
                                ros::Time(0), transform);
+      // listener.waitForTransform("/turtle2", "/turtle1",now, ros::Duration(3.0));
+      // listener.lookupTransform("/turtle2", "/turtle1",
+      //                          now, transform);
     }
     catch (tf::TransformException &ex) {
       ROS_ERROR("%s",ex.what());
@@ -38,7 +43,13 @@ int main(int argc, char** argv){
     vel_msg.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(), 2) +
                                   pow(transform.getOrigin().y(), 2));
     turtle_vel.publish(vel_msg);
-
+    // REVIEW Convert quarternion to rpy
+    double roll,pitch,yaw;
+    tf::Quaternion q=transform.getRotation();
+    tf::Matrix3x3(q).getRPY(roll,pitch, yaw);
+    std::cout<<roll<<std::endl;
+    std::cout<<pitch<<std::endl;
+    std::cout<<yaw<<std::endl;
     rate.sleep();
   }
   return 0;
