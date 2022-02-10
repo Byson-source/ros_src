0d1e8e9c605a84776c2687afdab39491da120bf7
Byson-source <byson0713@gmail.com>
2022-02-10 22:22:59 +0900

    rough transformationの実験用

--- src/cpp/src/relative_pose_estimation/RO.cpp
+++ src/cpp/src/relative_pose_estimation/RO.cpp
@@ -14,6 +14,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/eigen.hpp>
 
+#include <visualization_msgs/Marker.h>
+
 #include <ros/ros.h>
 #include <string>
 #include <vector>
@@ -22,7 +24,6 @@
 #include <math.h>
 // #include "calibration.h"
 // FIXME CMakeLists.txtを修正
-#include <tf/transform_listener.h>
 #include "nav_msgs/Path.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Point.h"
@@ -38,8 +39,9 @@ private:
     ros::Subscriber transformation_sub;
     ros::Publisher BA_pub;
     ros::Publisher rt_pub;
+    ros::Publisher marker_pub;
 
-    tf::TransformListener listener;
+    visualization_msgs::Marker points, line_strip, line_list;
 
     std::vector<int> loop_info;
     // NOTE xyz / xyzw
@@ -53,12 +55,14 @@ private:
     int info_index{0};
     int info_index_2{0};
 
+    int status{0};
+
     Eigen::Matrix4d transformation_result;
     Eigen::Matrix3d cam2robot;
     Eigen::Matrix3d robot2cam;
-    // Eigen::Matrix3d intrinsic_parameter;
-    // Eigen::Matrix4d img_to_cam_coordinate;
-    // cv::Mat camera_parameter;
+
+    Eigen::Matrix3d draw_rotation;
+    Eigen::Vector3d draw_t;
 
     // NOTE node_map...{time;{1:[]}
     // points location
@@ -82,6 +86,31 @@ public:
             0, 0, -1,
             1, 0, 0;
 
+        marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+
+        points.header.frame_id = line_list.header.frame_id = "robot1/map";
+        points.header.stamp = line_list.header.stamp = ros::Time::now();
+        points.ns = line_list.ns = "robot1";
+        points.action = line_list.action = visualization_msgs::Marker::ADD;
+        points.pose.orientation.w = line_list.pose.orientation.w = 1.0;
+
+        points.id = 0;
+        line_list.id = 2;
+
+        points.type = visualization_msgs::Marker::POINTS;
+        line_list.type = visualization_msgs::Marker::LINE_LIST;
+
+        // POINTS markers use x and y scale for width/height respectively
+        points.scale.x = 0.2;
+        points.scale.y = 0.2;
+
+        // LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
+        line_list.scale.x = 0.1;
+
+        points.color.r = 1.0f;
+        points.color.a = 1.0;
+        line_list.color.r = 1.0;
+        line_list.color.a = 1.0;
         // img_to_cam_coordinate << 0.0, 0.0, 1.0,
         //     -1.0, 0.0, 0.0,
         //     0.0, -1.0, 0.0;
@@ -123,28 +152,55 @@ public:
             pose.push_back(val.pose.orientation.w);
             path_2.push_back(pose);
         }
-        info_index_2 += 1;
+        std::cout << path_2.size() << std::endl;
         mapPath_dict_2[info_index_2] = path_2.size() - 1;
+        info_index_2 += 1;
+        if (status)
+        {
+            geometry_msgs::Point p;
+            geometry_msgs::Point p_prev;
+            points.points.clear();
+            line_list.points.clear();
+            for (size_t each{0}; each < path_2.size(); ++each)
+            {
+                Eigen::Vector3d p_mat(path_2[each][0], path_2[each][1], path_2[each][2]);
+                p_mat = draw_rotation * p_mat + draw_t;
+                p.x = p_mat.x();
+                p.y = p_mat.y();
+                p.z = p_mat.z();
 
-        // std::vector<double> pose;
+                points.points.push_back(p);
+
+                if (each > 0)
+                {
+                    Eigen::Vector3d p_prev_mat(path_2[each - 1][0], path_2[each - 1][1], path_2[each - 1][2]);
+                    p_prev_mat = draw_rotation * p_prev_mat + draw_t;
+                    line_list.points.push_back(p);
+                    p_prev.x = p_prev_mat.x();
+                    p_prev.y = p_prev_mat.y();
+                    p_prev.z = p_prev_mat.z();
+                    line_list.points.push_back(p_prev);
+                }
+            }
+            marker_pub.publish(points);
+            marker_pub.publish(line_list);
+        }
+        // lineの描画
     }
 
     void odom_CB(const cpp::HomogeneousArray::ConstPtr &data_)
     {
-        int number{0};
 
         transformation_result << data_->data[0], data_->data[1], data_->data[2], data_->data[3],
             data_->data[4], data_->data[5], data_->data[6], data_->data[7],
             data_->data[8], data_->data[9], data_->data[10], data_->data[11],
-            0, 0, 0, 1;
+            0.0, 0.0, 0.0, 1.0;
 
         // transformation_result=cam2robot*transformation_result;
 
-        // std::cout << transformation_result << std::endl;
-
         Eigen::Vector3d transfer_;
         Eigen::Matrix3d rotation_;
-        transfer_ = transformation_result.block(3, 0, 3, 1);
+        transfer_ = transformation_result.block(0, 3, 3, 1);
         rotation_ = transformation_result.block(0, 0, 3, 3);
 
         rotation_ = cam2robot.transpose() * rotation_ * cam2robot;
@@ -162,6 +218,10 @@ public:
         pose_result.euler = R;
 
         rt_pub.publish(pose_result);
+
+        draw_rotation = ans_r.matrix();
+        draw_t = ans_t;
+        status = 1;
     }
 
     void RO_CB(const cpp::FeatureArray::ConstPtr &data)
@@ -194,37 +254,6 @@ public:
                     img_coord_2.push_back(r2_coord);
                 }
             }
-
-            // if (who_detect == 1)
-            // {
-            //     feature_dict["R1"]["3d"].push_back(kp_loc_)
-            // }
-
-            // if (who_detect == 1)
-            //     result = RO_Estimator::pnp(kp_loc_r1_s, img_coord_2);
-            // else
-            //     result = RO_Estimator::pnp(kp_loc_r2_s, img_coord_1);
-
-            // Eigen::Matrix4   d result = RO_Estimator::pnp(who_detect, kp_loc_r1_s, kp_loc_r2_s);
-
-            // Eigen::Matrix3d R_ = result.block(0, 0, 3, 3);
-            // Eigen::Vector3d rpy = R_.eulerAngles(0, 1, 2);
-            // Eigen::Vector3d t_ = result.block(0, 3, 3, 1);
-            // std::cout << "=============================" << std::endl;
-            // std::cout << t_ << std::endl;
-            // std::cout << std::endl;
-
-            // cpp::RO_Array pose_result;
-            // Eigen::Vector3d ans_t = turnout_T(t_, who_detect);
-            // Eigen::Quaterniond ans_r = turnout_R(R_, who_detect);
-
-            // std::vector<double> R{ans_r.w(), ans_r.x(), ans_r.y(), ans_r.z()};
-            // std::vector<double> t{ans_t[0], ans_t[1], ans_t[2]};
-
-            // pose_result.translation = t;
-            // pose_result.euler = R;
-
-            // rt_pub.publish(pose_result);
         }
         else
         {
@@ -243,18 +272,18 @@ public:
         Eigen::Vector3d transfer_1_to_2;
         if (who_is_detect == 1)
         {
-            r1_img_index = loop_info[0];
-            r2_img_index = loop_info[1];
+            r1_img_index = (loop_info[0] - 1) * 0.5 + 1;
+            r2_img_index = loop_info[1] / 2;
             transfer_1_to_2 = transfer;
         }
         else
         {
-            r1_img_index = loop_info[1];
-            r2_img_index = loop_info[0];
-            transfer_1_to_2 = Eigen::Vector3d::Ones() - transfer;
+            r1_img_index = (loop_info[1] - 1) * 0.5 + 1;
+            r2_img_index = loop_info[0] / 2;
+            transfer_1_to_2 = Eigen::Vector3d::Zero() - transfer;
         }
-
         origin2r1 << path_1[mapPath_dict[r1_img_index]][0], path_1[mapPath_dict[r1_img_index]][1], path_1[mapPath_dict[r1_img_index]][2];
+
         origin2r2 << path_2[mapPath_dict_2[r2_img_index]][0], path_2[mapPath_dict_2[r2_img_index]][1], path_2[mapPath_dict_2[r2_img_index]][2];
 
         Eigen::Vector3d ans_t = transfer_1_to_2 - (origin2r2 - origin2r1);
@@ -273,14 +302,14 @@ public:
         if (who_is_detect == 1)
         {
             rotation_1TO2 = rotation_matrix;
-            r1_img_index = loop_info[0];
-            r2_img_index = loop_info[1];
+            r1_img_index = (loop_info[0] - 1) * 0.5 + 1;
+            r2_img_index = loop_info[1] / 2;
         }
         else
         {
             rotation_1TO2 = rotation_matrix.transpose();
-            r1_img_index = loop_info[1];
-            r2_img_index = loop_info[0];
+            r1_img_index = (loop_info[1] - 1) * 0.5 + 1;
+            r2_img_index = loop_info[0] / 2;
         }
 
         r1_q.x() = path_1[mapPath_dict[r1_img_index]][3];
@@ -298,35 +327,6 @@ public:
         Eigen::Quaterniond q_r1_to_r2 = (r2_q.inverse()) * rotation_1TO2 * r1_q;
         return q_r1_to_r2.normalized();
     }
-
-    // FIXME 前処理が必要
-    // Eigen::Matrix4d pnp(std::vector<cv::Point3d> kp_loc,
-    //                     std::vector<cv::Point2d> coord)
-    // {
-    //     // bearing vectors
-    //     // 3Dポイント集
-    //     std::cout << loop_info[0] << "->" << loop_info[1] << std::endl;
-
-    //     cv::Mat rotation_vector;
-    //     cv::Mat translation_vector;
-    //     cv::Mat dist_coeffs = cv::Mat::zeros(4, 1, cv::DataType<double>::type);
-
-    //     cv::solvePnP(kp_loc, coord, camera_parameter, dist_coeffs, rotation_vector, translation_vector);
-
-    //     Eigen::Vector3d translation;
-    //     Eigen::Matrix3d rotation;
-    //     cv::cv2eigen(rotation_vector, rotation);
-    //     cv::cv2eigen(translation_vector, translation);
-
-    //     rotation = rotation.transpose();
-    //     translation = rotation * (-translation);
-
-    //     Eigen::Matrix4d transformation;
-    //     transformation.block(0, 0, 3, 3) = rotation;
-    //     transformation.block(0, 3, 3, 1) = translation;
-
-    //     return transformation;
-    // }
 };
 
 int main(int argc, char **argv)
